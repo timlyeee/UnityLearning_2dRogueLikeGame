a7ff980009802e60c0ef26fc23749391bbc8f217
Link <timelyeee@gmail.com>
2020-05-04 18:18:31 +0200

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine ;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public float levelStartDelay = 2f;      //Time to wait before starting level, in seconds.
+    public float turnDelay = 0.1f;
+    public static GameManager instance = null;
+    private BoardManager boardScript;
+
+    private Text levelText;
+    private GameObject levelImage;
+    private int level = 1;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
+    private bool doingSetup;     //Boolean to check if we're setting up board, prevent Player from moving during setup.
+
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
+    void Awake() 
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+    //This is called each time a scene is loaded.
+    void OnLevelWasLoaded(int index)
+    {
+        //Add one to our level number.
+        level++;
+        //Call InitGame to initialize our level.
+        InitGame();
+    }
+    private void HideLevelImage()
+    {
+        levelImage.SetActive(false);
+        doingSetup = false; //So that the player can now  move.
+    }
+
+    void InitGame()
+    {
+        doingSetup = true;
+        levelImage = GameObject.Find("LevelImage");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        levelText.text = "Day " + level;
+        levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelStartDelay); // start the function after levelStartDelay time.
+
+        enemies.Clear();
+        boardScript.SetupScene(level);
+    }
+    public void GameOver()
+    {
+        levelText.text = "After " + level + " days, you starved";
+        levelImage.SetActive(true);
+        enabled = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (playersTurn || enemiesMoving || doingSetup)
+            return;
+        StartCoroutine(MoveEnemies());
+    }
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if(enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+        playersTurn = true;
+        enemiesMoving = false;
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource efxSource;
+    public AudioSource musicSource;
+    public static SoundManager instance = null;
+
+    public float lowPitchRange = .95f;
+    public float highPitchRange = 1.05f;
+    //standard pitch range is 1.0f, so these two are 5% high or low than standard one.
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void  PlaySingle(AudioClip clip)
+    {
+        efxSource.clip = clip;
+        efxSource.Play();
+    }
+    public void RandomizeSfx(params AudioClip [] clips)
+    {
+        int randomIndex = Random.Range(0, clips.Length);
+        float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+
+        efxSource.clip = clips[randomIndex];
+        efxSource.pitch = randomPitch;
+        efxSource.Play();
+    }
+}
